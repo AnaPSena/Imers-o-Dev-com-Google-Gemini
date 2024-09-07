5fdbbf18c3c4cca0cdee0646c4a8a2ea3f082dfb
Ana Paula Sena <102556199+AnaPSena@users.noreply.github.com>
2024-09-07 18:00:35 -0300

    Add files via upload

---
+++ dado.js
@@ -0,0 +1,22 @@
+let dados = [ 
+    {
+    titulo : "Rafaella Ballerini",
+    descricao : "Desenvolvedora de software, instrutora e criadora de conteúdo. Possui um canal com mais de 240 mil pessoas inscritas, onde ensina sobre os mais diversos temas relacionados a programação e tecnologia, como HTML, CSS, Javascript, Git, Github... É instrutora front-end na plataforma da Alura e também na Imersão Dev, projeto que leva o ensino inicial de programação para todo o Brasil gratuitamente.",
+    link : "https://cursos.alura.com.br/user/rafaella-ballerini",
+    tags: "Front end"
+},
+{
+    titulo : "Gabriela Vaz",
+    descricao : "Desenvolvedora Front-end com 49,3 mil seguidores no Instagram",
+    link : "https://github.com/gabevaz",
+    tags: "Front end"
+},
+{
+    titulo : " Glaucia Lemos",
+    descricao : "ela é desenvolvedora Cloud Advocate em JavaScript / Node.js e TypeScript na Microsoft. Além disso, realiza contribuições para projetos de código aberto no GitHub. Glaucia também pertence à WoMakersCode, uma organização focada em capacitar mulheres na tecnologia.",
+    link : "https://www.youtube.com/c/GlauciaLemos",
+    tags: "Front end"
+}
+]
+
+
